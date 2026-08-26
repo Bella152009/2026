325268d6a0a408b08adfd5c65bf8b00d8f89f3bb
Bella152009 <isabella.furman@escola.pr.gov.br>
2026-08-26 12:24:47 -0300

    Correct perguntas array structure
    Fixed syntax errors in the perguntas array.

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
+    {
     enunciado: "Pergunta 1",
     alternativas: [
         "Alternativa 1",
         "Alternativa 2"
-]
-  
-    "Pergunta 2"
+    ]
+},
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+
 ]
